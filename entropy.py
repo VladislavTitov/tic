85f829dcfc03e5fe571222413e14057f1b259b9e
VladislavTitov <titovvlad1997@yandex.ru>
2017-10-26 10:08:26 +0300

    entropy final version

--- entropy.py
+++ entropy.py
@@ -1,11 +1,12 @@
 from math import log2
 
-file = open("/home/vlados/Загрузки/Telegram Desktop/idiot", "r")
+file = open("/home/vlados/Загрузки/uliss.txt", "r")
 content = file.read()
 file.close()
 
 symbols_count = len(content)
 pairs_count = symbols_count - 1
+triplets_count = pairs_count - 1
 
 symbols = {}
 
@@ -14,6 +15,15 @@ def initPairs(symbols):
     for key in symbols.keys():
         pairs[key] = {x : 0 for x in symbols.keys()}
     return pairs
+
+def initTriplets(symbols):
+    triplets = {}
+    for key1 in symbols.keys():
+        triplets[key1] = {}
+        for key2 in symbols.keys():
+            triplets[key1][key2] = {x : 0 for x in symbols.keys()}
+    return triplets
+            
 
 for symbol in content:
     if symbol not in symbols.keys():
@@ -46,11 +56,39 @@ for key1, entry in pairs.items():
         pba = pairs[key1][key2] / pa1
         if pba > 0:
             HBa -= pba * log2(pba)
-        else:
-            HBa -= 0
     HBA += HBa * pa1
 print('H(B|A) = ' + str(HBA))
+
+
+triplets = initTriplets(symbols)
+
+for i in range(0, triplets_count):
+    triplets[content[i]][content[i + 1]][content[i + 2]] += 1
+
+for key1, entry1 in triplets.items():
+    for key2, entry2 in entry1.items():
+        for key3 in entry2:
+            entry2[key3] /= triplets_count
+
+HCBA = 0
+for key1, entry1 in triplets.items():
+    for key2, entry2 in entry1.items():
+        pab = pairs[key1][key2]
+        if pab > 0:
+            HCba = 0
+            for key3 in entry2.keys():
+                pcba = triplets[key1][key2][key3] / pab
+                if pcba > 0:
+                    HCba -= pcba * log2(pcba)
+            HCBA += HCba * pab
+        
+print('H(C|AB) = ' + str(HCBA))
 print('words = ' + str(symbols_count))
+            
+            
+            
+
+
         
         
         
